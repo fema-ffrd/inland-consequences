e84088b241103ebdd7b90ff151ac3b67bc5f8616
Troy Schmidt <schmidt.troy@gmail.com>
2025-11-04 10:55:18 -0500

    Imported SPHERE packages

---
+++ tests/__init__.py
@@ -0,0 +1,6 @@
+"""
+Test package initializer to make `tests` importable.
+
+This file is intentionally empty; it allows imports like
+`from tests.conftest import ...` during test collection/run.
+"""
