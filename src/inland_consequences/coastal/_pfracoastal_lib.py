273b57ff4cd551b9fcd9185de272b45f50d34b0b
Stimson <Alex.Stimson@stantec.com>
2025-12-04 13:49:22 -0500

    Added initial files for coastal tool conversion

---
+++ src/inland_consequences/coastal/_pfracoastal_lib.py
@@ -0,0 +1,11 @@
+import rasterio
+import geopandas as gpd
+import scipy
+import logging
+import typing
+import multiprocessing
+import os
+
+class _PFRACoastal_Lib:
+    def __init__(self) -> object:
+        pass
