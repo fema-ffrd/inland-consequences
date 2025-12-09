f12f817dfaf2bdfea9960ad5cf0612166f289811
Stimson <Alex.Stimson@stantec.com>
2025-12-09 16:39:06 -0500

    Added function 'getZscore'

--- src/inland_consequences/coastal/_pfracoastal_lib.py
+++ src/inland_consequences/coastal/_pfracoastal_lib.py
@@ -9,3 +9,18 @@ import os
 class _PFRACoastal_Lib:
     def __init__(self) -> object:
         pass
+    
+    ###################
+    # getZscore()
+    # in:
+    #	x = data value
+    #	mean_data = mean of data
+    #	sd_data = 1 std dev of data
+    # out:
+    #	zscore
+    # called by:
+    # 	main()
+    # calls:
+    #	NULL
+    def getZscore(x: float, mean_data: float, sd_data: float) -> float:
+        return (x - mean_data) / sd_data
