b2c914588a0ee292d1d2595b36ea8a7430924ec9
Stimson <Alex.Stimson@stantec.com>
2025-12-09 15:51:36 -0500

    Added function 'adjust_Loss_DEDLIM1'

--- src/inland_consequences/coastal/_pfracoastal_lib.py
+++ src/inland_consequences/coastal/_pfracoastal_lib.py
@@ -1,5 +1,6 @@
 import rasterio
 import geopandas as gpd
+import numpy as np
 import scipy
 import logging
 import typing
@@ -9,3 +10,31 @@ import os
 class _PFRACoastal_Lib:
     def __init__(self) -> object:
         pass
+    
+    #####################
+    # adjust_Loss_DEDLIM1()
+    #	adjusts raw loss values for insurance deductible and limit.
+    #	adjusted loss = raw loss minus the deductible, then capped at limit.
+    # in:
+    #	loss = raw loss value
+    #	ded = deductible
+    #	lim = limit
+    # out:
+    #	adjusted loss
+    # called by:
+    #	runMC_AALU_x4()
+    # calls:
+    #	NULL
+    def adjust_Loss_DEDLIM1(loss: int, ded=0, lim=100_000_000) -> int:
+        dLoss = loss - ded
+        if dLoss < 0:
+            dLoss = 0
+        lLoss = min([val for val in [dLoss,lim] if val not in (np.nan, None)])
+        return lLoss
+    # Example,
+    ## 
+    # > bldg_loss = 25_000
+    # > bldg_ded = 5000
+    # > bldg_lim = 250_000
+    # > adjust_Loss_DEDLIM1(bldg_loss, bldg_ded, bldg_lim)
+    ## [1] 20000
