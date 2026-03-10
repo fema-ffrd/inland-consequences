24d77fbb60d21fc3a2247d9cb5ab84d14aec962f
daniel-aragon-MBI <daniel.aragon@mbakerintl.com>
2026-03-10 15:49:45 -0600

    api reference updates

--- packages/core/src/sphere/core/schemas/__init__.py
+++ packages/core/src/sphere/core/schemas/__init__.py
@@ -5,7 +5,7 @@ from sphere.core.schemas.field_mapping import FieldMapping
 from sphere.core.schemas.abstract_raster_reader import AbstractRasterReader
 from sphere.core.schemas.abstract_vulnerability_function import AbstractVulnerabilityFunction
 from sphere.core.schemas.fast_buildings import FastBuildings
-from sphere.core.schemas.nsi_buildings import NSIBuildings
+from sphere.core.schemas.nsi_buildings import NsiBuildings
 
 __all__ = [
     "Buildings",
@@ -13,5 +13,5 @@ __all__ = [
     "AbstractRasterReader",
     "AbstractVulnerabilityFunction",
     "FastBuildings",
-    "NSIBuildings",
+    "NsiBuildings",
 ]
